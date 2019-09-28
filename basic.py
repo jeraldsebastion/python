bab576ca59a931230c29c3ecf63373baea2d8560
Jerald MS <jeraldsebastion@gmail.com>
2019-09-28 13:57:49 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,49 @@
+#for printing 
+print("hello")
+
+#its the correct order to put if satement
+if 5>2:
+
+    print("five is greater the 2")
+
+# if this like its wrong
+ #  if 5>2:
+ # print ("hello")
+
+#this write sytax mutiple line
+if 5>2:
+
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+"""
+ wrong
+if >2:
+
+   print("Five is greater than two!")
+     print("Five is greater than two!")
+
+"""
+"""
+Variables are containers for storing data values.
+
+Unlike other programming languages, Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it.
+"""
+x=5
+y="john"
+
+print(x)
+print(y)
+#Variables do not need to be declared with any particular type and can even change type after they have been set.
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+
+
+
+
+
